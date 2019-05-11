4bb161c351df7b183a370a1718f55f703dc67227
Soumya Deb <soumyadeb1809@gmail.com>
2019-05-12 01:55:17 +0530

    Initial Commit

---
+++ src/main/java/com/monkoauth/MonkOAuthApplication.java
@@ -0,0 +1,13 @@
+package com.monkoauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonkOAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonkOAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/monkoauth/MonkOAuthApplicationTests.java
@@ -0,0 +1,16 @@
+package com.monkoauth;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MonkOAuthApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
